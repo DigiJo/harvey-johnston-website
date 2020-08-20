381717b85765f9aa566ccfe477ef86a19b525634
Joe Johnston <joejohnston1989@gmail.com>
2020-08-20 18:58:01 +0100

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,7 @@
                 </g-link>
             </div>
             <nav class="nav right">
-                <g-link class="nav__link" to="/journal">Memories 📚</g-link>
+                <g-link class="nav__link" to="/journal">Memories 💭</g-link>
                 <g-link class="nav__link" to="/contact">Say Hi 👋</g-link>
             </nav>
         </div>
